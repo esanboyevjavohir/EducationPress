a648b7b026d5b47ff00fd1ca7dbadd85a7c475b4
Developer04 <javohiresanboyev053@gmail.com>
2025-07-12 20:51:43 +0500

    Continue the project

---
+++ src/EduPress.Application/Helpers/GenerateJWT/IJwtTokenHandler.cs
@@ -0,0 +1,11 @@
+﻿using EduPress.Core.Entities;
+
+namespace EduPress.Application.Helpers.GenerateJWT
+{
+    public interface IJwtTokenHandler
+    {
+        string GenerateAccessToken(User user);
+        string GenerateAccessToken(User user, string sessionToken);
+        string GenerateRefreshToken(); 
+    }
+}
